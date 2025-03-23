ba7971534ff4324f372f65e1850f366db1908675
Muxy1108 <2175968350@qq.com>
2025-03-23 23:37:04 +0800

    20250323

--- src/checker.java
+++ src/checker.java
@@ -1,69 +1,94 @@
+import java.util.Objects;
 import java.util.Scanner;
+import static java.lang.Character.isLetter;
 
 public class checker {
     public static void main(String[] args) {
-        String origin;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string");
-        origin = sc.nextLine();
 
-        StringBuilder s = new StringBuilder();
+        String origin = "0";
+        int AtomCounter = 0;
 
-        for(int i = 0;i < origin.length();i ++) {
-            char tmp = origin.charAt(i);
-            if (tmp == ' ') continue;
-            if (true) { // check the letter validity
-                s.append(tmp);
+        while(origin != "-1") {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a string,or enter -1 to exit");
+            origin = sc.nextLine();
+            StringBuilder s = new StringBuilder();
+
+            if(Objects.equals(origin, "-1")) {
+                System.out.println("-------THE ENDING-------");
+                break;
             }
 
-        }
+            for (int i = 0; i < origin.length(); i++) {
+                char tmp = origin.charAt(i);
+                if (tmp == ' ') continue;
+                if (isValid(tmp)) s.append(tmp);
+            }
 
+            boolean isWFF = true;
+            int NumBra = 0;
+            int LastChar = 0;
 
-
-
-        boolean isWFF = true;
-        int NumBra = 0;
-        int LastChar = 0;
         /*
         1 : atom
         2 : operator
         3 : parentheses
         */
 
-        for(int i = 0;i < s.length();i ++){
+            if(s.charAt(0) != '(' || s.charAt(s.length() - 1) != ')') {
+                isWFF = false;
+            }
+            if(isWFF){
+                for(int i = 0; i < s.length();i ++) {
+                    char tmp = s.charAt(i);
 
-            if(s.charAt(i) == '(') {
-                NumBra ++;
-                LastChar = 3;
+                    if(tmp == '('){
+                        NumBra ++;
+                        LastChar = 3;
+                        continue;
+                    }
+                    if (tmp == ')') {
+                        NumBra --;
+                        LastChar = 3;
+                        continue;
+                    }
+                    if (NumBra < 0) {
+                        isWFF = false;
+                        break;
+                    }
+                    //Numbra always >= 0
 
+                    if (isLetter(tmp)) {
+                        if (LastChar == 1) {
+                            isWFF = false;
+                            break;
+                        }
+                        LastChar = 1;
+                        AtomCounter ++;
+                        continue;
+                    }
+
+                    if (tmp == '∧' || tmp == '∨' || tmp == '→' || tmp == '↔' || tmp == '¬') {
+                        LastChar = 2;
+                        continue;
+                    }
+                }
             }
 
-            if(s.charAt(i) == ')'){
-                NumBra --;
-                LastChar = 3;
-            }
+            if(NumBra != 0) isWFF = false;
+            if(AtomCounter == 0) isWFF = false;
 
-
-
-            //Numbra always >= 0
-
-
-
-
-
-
-
+            //output
+            if (isWFF) System.out.println(s + " is a WFF");
+            else       System.out.println(s + " is not a WFF");
 
         }
 
+    }
 
-
-
-
-
-
-
-
-
+    private static boolean isValid(char ch) {
+        return isLetter(ch) || ch == '∧' || ch == '∨' || ch == '→' ||
+                ch == '↔' || ch == '(' || ch == ')' || ch == '¬';
     }
 }
